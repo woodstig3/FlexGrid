b3b7908f512c18d9079f8d57481b6dd4098b9ef7
Richard Dai <dai_richard@hotmail.com>
2024-07-24 10:39:40 +0800

    fetch_file_ macro disabled because not used in customer test

--- WSS_Main/src/GlobalVariables.h
+++ WSS_Main/src/GlobalVariables.h
@@ -12,7 +12,7 @@
 
 #define _DEVELOPMENT_MODE_				// <---------- Uncomment it during production mode, Comment if when launching to customer
 #define _DEVELOPMENT_MODE_EXTRA_		// for gap , P1, P2, etc
-#define _FETCH_PATTERN_					//drc need to be disabled before serial interface connect with customer line card
+//#define _FETCH_PATTERN_					//drc need to be disabled before serial interface connect with customer line card
 //#define _TWIN_WSS_						// If not defined then only Module.1 will exist for user to set.
 
 #define PI 3.141592653589793238
